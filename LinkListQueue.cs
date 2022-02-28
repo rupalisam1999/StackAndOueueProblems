e65256145b4845605d971e370a832567425efbc9
rupalisam1999 <rupalisangale1999@gmail.com>
2022-02-28 06:59:18 +0530

    [Rupali]ADDED: Create a simple Queue

---
+++ LinkListQueue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueueProblems
+{
+    class LinkListQueue
+    {
+        Node head = null;
+
+       
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("\n inserted into queue" + node.data);
+        }
+
+       
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
+    
+
